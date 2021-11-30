0d0b9a5a2d99780384e27118766e868151a8e812
alan almeida <flamigeral2@gmail.com>
2021-11-29 19:29:00 -0500

    Agregar archivos de proyecto.

---
+++ LlamandoDesdeWin/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace LlamandoDesdeWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSUMA = new System.Windows.Forms.TextBox();
+            this.txtFACTO = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.sum1 = new System.Windows.Forms.TextBox();
+            this.sum2 = new System.Windows.Forms.TextBox();
+            this.facto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resultado 1, suma";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Resultado 2, factorial";
+            // 
+            // txtSUMA
+            // 
+            this.txtSUMA.Location = new System.Drawing.Point(299, 159);
+            this.txtSUMA.Name = "txtSUMA";
+            this.txtSUMA.Size = new System.Drawing.Size(216, 22);
+            this.txtSUMA.TabIndex = 2;
+            // 
+            // txtFACTO
+            // 
+            this.txtFACTO.Location = new System.Drawing.Point(299, 256);
+            this.txtFACTO.Name = "txtFACTO";
+            this.txtFACTO.Size = new System.Drawing.Size(216, 22);
+            this.txtFACTO.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(313, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "TAREA";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 78);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "OPERAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(90, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(185, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ingrese el 1er valor a sumar";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(90, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(188, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Ingrese el 2do valor a sumar";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(90, 212);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Ingrese el factorial";
+            // 
+            // sum1
+            // 
+            this.sum1.Location = new System.Drawing.Point(299, 71);
+            this.sum1.Name = "sum1";
+            this.sum1.Size = new System.Drawing.Size(100, 22);
+            this.sum1.TabIndex = 9;
+            // 
+            // sum2
+            // 
+            this.sum2.Location = new System.Drawing.Point(299, 110);
+            this.sum2.Name = "sum2";
+            this.sum2.Size = new System.Drawing.Size(100, 22);
+            this.sum2.TabIndex = 10;
+            // 
+            // facto
+            // 
+            this.facto.Location = new System.Drawing.Point(299, 206);
+            this.facto.Name = "facto";
+            this.facto.Size = new System.Drawing.Size(100, 22);
+            this.facto.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.facto);
+            this.Controls.Add(this.sum2);
+            this.Controls.Add(this.sum1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFACTO);
+            this.Controls.Add(this.txtSUMA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSUMA;
+        private System.Windows.Forms.TextBox txtFACTO;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox sum1;
+        private System.Windows.Forms.TextBox sum2;
+        private System.Windows.Forms.TextBox facto;
+    }
+}
+
